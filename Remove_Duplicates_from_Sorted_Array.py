201a0938557e7c89d8fe28c4ba38e4d712f73f5b
ofir-frd <85901822+ofir-frd@users.noreply.github.com>
2021-12-13 22:36:15 +0200

    Update Remove_Duplicates_from_Sorted_Array.py

--- Remove_Duplicates_from_Sorted_Array.py
+++ Remove_Duplicates_from_Sorted_Array.py
@@ -4,7 +4,7 @@ Remove Duplicates from Sorted Array
 
 import numpy as np
 
-def removeDuplicates(self, nums: list):
+def removeDuplicates(nums: list):
     i=0
     while i < (len(nums) - 1):
         if nums[i] == nums[i + 1]:
