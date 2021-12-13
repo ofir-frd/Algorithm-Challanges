85ac6b1a71c716d023105d54bd47787bc65a67c8
ofir-frd <85901822+ofir-frd@users.noreply.github.com>
2021-12-13 17:17:53 +0200

    Add files via upload

---
+++ Remove_Duplicates_from_Sorted_Array.py
@@ -0,0 +1,18 @@
+"""
+Remove Duplicates from Sorted Array
+"""
+
+import numpy as np
+
+class Solution:
+    def removeDuplicates(self, nums: list):
+
+        i=0
+        while i < (len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                nums.pop(i)
+                continue
+            i += 1
+
+        print(nums)
+     
